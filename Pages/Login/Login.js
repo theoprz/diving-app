05aa79c055701825ad8b225d18990870dfd27995
Quentin <quentin.pruvot@student.yncrea.fr>
2023-06-13 11:18:54 +0200

    Login style

--- Pages/Login/Login.js
+++ Pages/Login/Login.js
@@ -1,30 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Pressable, Dimensions, useWindowDimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import NavBar from "../../Component/Header/Header";
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigation = useNavigation();
-    const [token, setToken] = useState();
+    const [token, setToken] = useState('');
 
-    useEffect(async() => {
-        const findToken = await AsyncStorage.getItem('token');
-        if (findToken !== null) {
-            fetch('http://93.104.215.68:5000/api/users/verify', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-access-token': findToken
-                },
-                body: JSON.stringify({ token: findToken })
-            }).then((response) => {
-                if (response.ok) {
-                    navigation.navigate('Home');
-                }
-            });
-        }
+    useEffect(() => {
+        const fetchData = async () => {
+            const findToken = await AsyncStorage.getItem('token');
+            if (findToken !== null) {
+                fetch('http://93.104.215.68:5000/api/users/verify', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'x-access-token': findToken
+                    },
+                    body: JSON.stringify({ token: findToken })
+                }).then((response) => {
+                    if (response.ok) {
+                        navigation.navigate('Home');
+                    }
+                });
+            }
+        };
+
+        fetchData();
     }, []);
 
     const handleSubmit = async () => {
@@ -48,31 +53,35 @@ export default function Login() {
         }
     };
 
+    const windowDimensions = useWindowDimensions();
+
+
     return (
-        <View style={styles.container}>
+            <View style={styles.container}>
             <Text style={styles.title}>Login</Text>
             <View style={styles.form}>
                 <View style={styles.inputContainer}>
-                    <Text style={styles.label}>Email :</Text>
                     <TextInput
                         style={styles.input}
                         keyboardType="email-address"
                         onChangeText={setEmail}
+                        placeholder="Your Email"
+                        placeholderTextColor="#888"
                     />
                 </View>
                 <View style={styles.inputContainer}>
-                    <Text style={styles.label}>Password :</Text>
                     <TextInput
                         style={styles.input}
                         secureTextEntry
                         onChangeText={setPassword}
+                        placeholder="Your Password"
+                        placeholderTextColor="#888"
                     />
                 </View>
-                <Button
-                    title="Login"
-                    onPress={handleSubmit}
-                    style={styles.button}
-                />
+
+                <Pressable style={styles.button} onPress={handleSubmit}>
+                    <Text style={styles.login}>Login</Text>
+                </Pressable>
             </View>
         </View>
     );
@@ -82,30 +91,51 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        height: '100%',
     },
     title: {
-        fontSize: 20,
+        fontSize: 24,
         fontWeight: 'bold',
-        marginBottom: 24
+        marginBottom: 24,
     },
     form: {
-        width: '80%'
+        width: '80%',
     },
     inputContainer: {
-        marginBottom: 16
+        marginBottom: 16,
     },
     label: {
         fontSize: 16,
-        marginBottom: 8
+        marginBottom: 8,
     },
     input: {
         height: 40,
-        borderColor: 'gray',
         borderWidth: 1,
-        paddingHorizontal: 10
+        borderColor: '#ccc',
+        paddingHorizontal: 10,
+        borderRadius: 8,
+        color: '#000',
     },
     button: {
-        marginTop: 16
-    }
+        marginTop: 16,
+        marginLeft: '35%',
+        marginRight: '35%',
+        width: '30%',
+        borderRadius: 25,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 3,
+        backgroundColor: '#2811df',
+    },
+    login: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: '#fff',
+    },
+
 });
